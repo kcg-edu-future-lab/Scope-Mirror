a189e802db3f7df46a285c11f723fb0490929655
Keiho Sakapon <git@saka-pon.net>
2017-06-27 19:16:54 +0900

    Add classes

---
+++ ClipMirror-Single/ClipMirror.Single/AppModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using Reactive.Bindings;
+
+namespace ClipMirror.Single
+{
+    public class AppModel
+    {
+        public static AppModel Instance { get; } = new AppModel();
+
+        public DisplayScreen[] Screens { get; }
+        public ReactiveProperty<DisplayScreen> SelectedScreen { get; } = new ReactiveProperty<DisplayScreen>();
+
+        public ReactiveProperty<bool> IsDisplaying { get; } = new ReactiveProperty<bool>(mode: ReactivePropertyMode.DistinctUntilChanged);
+
+        public AppModel()
+        {
+            Screens = Screen.AllScreens
+                .Select((s, i) => new DisplayScreen(i + 1, s))
+                .ToArray();
+            SelectedScreen.Value = Screens.FirstOrDefault(s => !s.Screen.Primary) ?? Screens.First();
+        }
+    }
+
+    public class DisplayScreen
+    {
+        public int Id { get; }
+        public Screen Screen { get; }
+
+        public Int32Rect Rectangle { get; }
+        public string Info => $"{Id}: {Rectangle}";
+
+        public DisplayScreen(int id, Screen screen)
+        {
+            Id = id;
+            Screen = screen;
+
+            Rectangle = screen.Bounds.ToInt32Rect();
+        }
+    }
+}
---
+++ ClipMirror-Single/ClipMirror.Single/Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using DRectangle = System.Drawing.Rectangle;
+
+namespace ClipMirror.Single
+{
+    public static class Convert
+    {
+        public static Int32Rect ToInt32Rect(this DRectangle r)
+        {
+            return new Int32Rect(r.X, r.Y, r.Width, r.Height);
+        }
+
+        public static DRectangle ToRectangle(this Int32Rect r)
+        {
+            return new DRectangle(r.X, r.Y, r.Width, r.Height);
+        }
+    }
+}
