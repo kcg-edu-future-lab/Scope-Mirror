e62f71dbbcebcad9db03e4f7388c3a4945d1da8a
Keiho Sakapon <git@saka-pon.net>
2017-06-27 23:31:18 +0900

    Get clipped screen images by timer

--- ClipMirror-Single/ClipMirror.Single/AppModel.cs
+++ ClipMirror-Single/ClipMirror.Single/AppModel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Windows;
 using System.Windows.Forms;
 using Reactive.Bindings;
@@ -11,6 +15,9 @@ namespace ClipMirror.Single
     {
         public static AppModel Instance { get; } = new AppModel();
 
+        public Int32Rect ClipBounds { get; set; } = new Int32Rect(100, 100, 600, 400);
+        public ReadOnlyReactiveProperty<byte[]> ScreenImage { get; }
+
         public DisplayScreen[] Screens { get; }
         public ReactiveProperty<DisplayScreen> SelectedScreen { get; } = new ReactiveProperty<DisplayScreen>();
 
@@ -18,10 +25,26 @@ namespace ClipMirror.Single
 
         public AppModel()
         {
+            ScreenImage = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(0.2))
+                .Select(_ => GetClippedScreenImage())
+                .ToReadOnlyReactiveProperty();
+
             Screens = Screen.AllScreens
                 .Select((s, i) => new DisplayScreen(i + 1, s))
                 .ToArray();
             SelectedScreen.Value = Screens.FirstOrDefault(s => !s.Screen.Primary) ?? Screens.First();
+        }
+
+        byte[] GetClippedScreenImage()
+        {
+            using (var bitmap = new Bitmap(ClipBounds.Width, ClipBounds.Height))
+            using (var graphics = Graphics.FromImage(bitmap))
+            using (var memory = new MemoryStream())
+            {
+                graphics.CopyFromScreen(ClipBounds.X, ClipBounds.Y, 0, 0, bitmap.Size);
+                bitmap.Save(memory, ImageFormat.Png);
+                return memory.ToArray();
+            }
         }
     }
 
