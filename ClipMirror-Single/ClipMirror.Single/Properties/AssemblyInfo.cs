2027092fd29287b8c7e7812dccce4fc76d70f8d3
Keiho Sakapon <git@saka-pon.net>
2017-06-27 18:34:01 +0900

    Update project settings

--- ClipMirror-Single/ClipMirror.Single/Properties/AssemblyInfo.cs
+++ ClipMirror-Single/ClipMirror.Single/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ClipMirror.Single")]
+[assembly: AssemblyTitle("Clip Mirror - Single")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClipMirror.Single")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2017 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
