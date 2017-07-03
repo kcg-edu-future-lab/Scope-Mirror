b02eebb7402fd0e34a747b9ba10b53ee7aa64e61
Keiho Sakapon <git@saka-pon.net>
2017-07-03 15:57:02 +0900

    Add tools

---
+++ Tools/CreateZipForAssembly-cs.ps1
@@ -0,0 +1,36 @@
+﻿# source assembly file path
+if (-not ($Args[0])) { return 100 }
+# target dir path
+if (-not ($Args[1])) { return 101 }
+
+$references = @("System.IO.Compression.FileSystem")
+$source = @"
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+public static class ZipHelper
+{
+    public static int CreateZipForAssembly(string sourceAssemblyFilePath, string targetDirPath)
+    {
+        var assemblyName = Path.GetFileNameWithoutExtension(sourceAssemblyFilePath);
+        var assembly = Assembly.LoadFrom(sourceAssemblyFilePath);
+        var assemblyFileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+        if (assemblyFileVersion == null) return 200;
+
+        var sourceDirPath = Path.GetDirectoryName(sourceAssemblyFilePath);
+        var targetZipFileName = string.Format("{0}-{1}.zip", assemblyName, assemblyFileVersion.Version);
+        var targetZipFilePath = Path.Combine(targetDirPath, targetZipFileName);
+
+        Directory.CreateDirectory(targetDirPath);
+        File.Delete(targetZipFilePath);
+        ZipFile.CreateFromDirectory(sourceDirPath, targetZipFilePath);
+
+        return 0;
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp -ReferencedAssemblies $references
+[ZipHelper]::CreateZipForAssembly($Args[0], $Args[1])
---
+++ Tools/IncrementVersion-cs.ps1
@@ -0,0 +1,54 @@
+﻿$source = @"
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        if (args.Length < 1) return 100;
+
+        IncrementVersion(args[0]);
+        return 0;
+    }
+
+    public static void IncrementVersion(string projDirPath)
+    {
+        var assemblyInfoPath = Directory.EnumerateFiles(projDirPath, "AssemblyInfo.cs", SearchOption.AllDirectories).First();
+        var contents = File.ReadLines(assemblyInfoPath, Encoding.UTF8)
+            .Select(IncrementLine)
+            .ToArray();
+        File.WriteAllLines(assemblyInfoPath, contents, Encoding.UTF8);
+    }
+
+    static string IncrementLine(string line)
+    {
+        if (line.StartsWith("//")) return line;
+
+        var match = Regex.Match(line, @"Assembly(File)?Version\(""([0-9\.]+)""\)");
+        if (!match.Success) return line;
+
+        var oldVersion = match.Groups[2].Value;
+        var newVersion = IncrementBuildNumber(oldVersion);
+        return line.Replace(oldVersion, newVersion);
+    }
+
+    static string IncrementBuildNumber(string version)
+    {
+        return Regex.Replace(version, @"^(\d+\.\d+\.)(\d+)((\.\d+)?)$", m => m.Groups[1].Value + IncrementNumber(m.Groups[2].Value) + m.Groups[3].Value);
+    }
+
+    static string IncrementNumber(string i)
+    {
+        return (int.Parse(i) + 1).ToString();
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp
+
+# $Args[0]: project directory path
+[Program]::Main($Args)
