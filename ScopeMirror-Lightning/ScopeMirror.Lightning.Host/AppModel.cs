4e9d803b408a4d9b0b5aefae3fc4774ee03c8dd0
Keiho Sakapon <git@saka-pon.net>
2017-07-06 23:52:00 +0900

    Add classes

---
+++ ScopeMirror-Lightning/ScopeMirror.Lightning.Host/AppModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Reactive.Bindings;
+
+namespace ScopeMirror.Lightning.Host
+{
+    public class AppModel
+    {
+        public static AppModel Instance { get; } = new AppModel();
+
+        public ReactiveProperty<byte[]> ScreenImage { get; } = new ReactiveProperty<byte[]>();
+    }
+}
