c662abd50848a930ea6d5a1764b3c3d10046ab09
Keiho Sakapon <git@saka-pon.net>
2017-07-10 16:23:44 +0900

    Add build tools

---
+++ Tools/BuildApp-Lightning.ps1
@@ -0,0 +1,15 @@
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+.\IncrementVersion-cs.ps1 ..\ScopeMirror-Lightning\ScopeMirror.Lightning.Guest
+.\IncrementVersion-cs.ps1 ..\ScopeMirror-Lightning\ScopeMirror.Lightning.Host
+
+$slnFilePath = "..\ScopeMirror-Lightning\ScopeMirror-Lightning.sln"
+& $msbuild $slnFilePath /p:Configuration=Release /t:Clean
+& $msbuild $slnFilePath /p:Configuration=Release /t:Rebuild
+
+# In case of PowerShell, the work directory is that of .ps1 file.
+.\CreateZipForAssembly-cs.ps1 ..\ScopeMirror-Lightning\ScopeMirror.Lightning.Guest\bin\Release\ScopeMirror.Lightning.Guest.exe ..\Downloads
+.\CreateZipForAssembly-cs.ps1 ..\ScopeMirror-Lightning\ScopeMirror.Lightning.Host\bin\Release\ScopeMirror.Lightning.Host.exe ..\Downloads
+
+explorer ..\Downloads
